468d7d0339b0460387e9ec567ad59fd61b2907e5
Paritosh456Pancholi <paritoshpancholi2@gmail.com>
2022-06-06 17:59:42 +0530

    commit

---
+++ largest_element.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+void largest(int arr[],int n ,int s, int e){
+    for(int j=0;j<n;j++){
+
+    }
+}    
+int main() {
+    
+    int n;
+    cin>>n;
+    int arr[];
+    int s,e;
+    for(int i=0; i < n; i++)
+       {
+           cout<<arr[i]<<" ";
+       }
+    cout<<endl;
+    largest(arr,n,s,e);
+    return 0;
+    
+}
---
+++ operation_on_array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void insert(int arr[])    
+int main() {
+    int n;
+    cin>>n;
+    int arr[],int x,pos,cap;
+    for(int i=0; i < n; i++)
+       {
+           cout<<arr[i]<<" ";
+       }
+    cout<<endl;
+    insert(arr,n,x,pos,cap);
+    return 0;
+    
+}
